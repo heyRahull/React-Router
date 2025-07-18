f8381bb8a8d20686e60a45becca4a1c031011ba0
Rahul Agarwal <rahul123agarwal99@gmail.com>
2025-07-18 17:33:24 +0530

    Cleanup for Tutorial 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import About from "./About"
-import Home from "./Home"
-import { Routes, Route } from "react-router"
-
 function App() {
 
   return (
     <div>
       <h1>React Router Setup</h1>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>}/>
-      </Routes>
     </div>
   )
 }
