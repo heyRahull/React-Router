c885460ccfc3382ff1161409cd46fca4e5fbf137
Rahul Agarwal <rahul123agarwal99@gmail.com>
2025-07-22 16:28:58 +0530

    Tutorial 4 - Creating Navbar/header using React router

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router'
+import './Navbar.css'
 
 export const Navbar = () => {
   return (
-    <div>
-        <h3><Link to="/">Home</Link></h3>
-      <h3><Link to="/about">About</Link></h3>
-      <h3><Link to="/login">Login</Link></h3>
+    <div className='header'>
+       <div><Link to="/" className='link'><h2>Logo</h2></Link></div>
+       <div>
+        <ul>
+          <li><Link to="/about" className='link'>About</Link></li>
+          <li><Link to="login" className='link'>Login</Link></li>
+          <li><Link to="/tutorial" className='link'>Tutorials</Link></li>
+        </ul>
+       </div>
     </div>
   )
 }
